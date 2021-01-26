f951044ef081200329785a0c8aea6569dc0e0224
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2021-01-26 09:26:30 -0300

    refactor: atualiza

--- RecursivePartial.d.ts
+++ RecursivePartial.d.ts
@@ -1,3 +1,10 @@
-type RecursivePartial<T> = {
-  [P in keyof T]?: RecursivePartial<T[P]>;
+type primitive = string | number | boolean | undefined | null;
+type DeepReadonly<T> = T extends primitive ? T : DeepReadonlyObject<T>;
+type DeepReadonlyObject<T> = {
+  readonly [P in keyof T]: DeepReadonly<T[P]>;
+};
+
+type DeepPartial<T> = T extends primitive ? T : DeepPartialObject<T>;
+type DeepPartialObject<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
 };
