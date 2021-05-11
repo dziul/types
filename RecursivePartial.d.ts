3317ee612c7424eacf410fa4c9445a592588dc5d
Luiz Carlos Wagner <luizcarloswagner@gmail.com>
2021-05-11 17:46:02 -0300

    refactor: atualiza nomeclatura

--- RecursivePartial.d.ts
+++ RecursivePartial.d.ts
@@ -1,10 +1,12 @@
-type primitive = string | number | boolean | undefined | null;
-type DeepReadonly<T> = T extends primitive ? T : DeepReadonlyObject<T>;
+type Primitive = string | number | boolean | undefined | null;
+type DeepReadonly<T> = T extends Primitive ? T : DeepReadonlyObject<T>;
 type DeepReadonlyObject<T> = {
-  readonly [P in keyof T]: DeepReadonly<T[P]>;
+	readonly [P in keyof T]: DeepReadonly<T[P]>;
 };
 
-type DeepPartial<T> = T extends primitive ? T : DeepPartialObject<T>;
+type DeepPartial<T> = T extends Primitive ? T : DeepPartialObject<T>;
 type DeepPartialObject<T> = {
-  [P in keyof T]?: DeepPartial<T[P]>;
+	[P in keyof T]?: DeepPartial<T[P]>;
 };
+
+type ValueOf<T> = T[keyof T];
